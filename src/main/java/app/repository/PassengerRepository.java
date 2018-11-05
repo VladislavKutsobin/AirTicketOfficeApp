689874b7530711065de7c9bbac6d8baeb5d35fa4
Vladislav Kutsobin <kutsobin_vladislav@rambler.ru>
2018-11-05 20:18:41 +0300

    Fix PassengerRepo

--- src/main/java/app/repository/PassengerRepository.java
+++ src/main/java/app/repository/PassengerRepository.java
@@ -2,13 +2,5 @@ package main.java.app.repository;
 
 import main.java.app.model.Passenger;
 
-import java.util.List;
-
-public interface PassengerRepository {
-    void insert(Passenger passenger);
-    void delete(int id);
-    List<Passenger> findAll();
-
-    Passenger getById(int id);
-
+public interface PassengerRepository extends GenericRepository<Passenger, Integer>{
 }
