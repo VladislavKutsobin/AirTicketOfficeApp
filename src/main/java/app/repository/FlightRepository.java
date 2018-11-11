467cd23c1c74b5907f3efcf346236b99978df02f
Vladislav Kutsobin <kutsobin_vladislav@rambler.ru>
2018-11-12 02:02:08 +0300

    Add exceptions

--- src/main/java/app/repository/FlightRepository.java
+++ src/main/java/app/repository/FlightRepository.java
@@ -2,11 +2,12 @@ package main.java.app.repository;
 import main.java.app.model.Flight;
 import main.java.app.model.Passenger;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface FlightRepository extends GenericRepository<Flight, Integer> {
 
-    List<Flight> searchByDeparture(String departurePoint);
-    List<Flight> searchByArrival(String arrivalPoint);
-    List<Flight> searchByDate(String date);
+    List<Flight> searchByDeparture(String departurePoint) throws IOException;
+    List<Flight> searchByArrival(String arrivalPoint) throws IOException;
+    List<Flight> searchByDate(String date) throws IOException;
 }
