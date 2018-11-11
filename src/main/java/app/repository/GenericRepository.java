aa5f379a176503d1cb0288d19a971dee6a2db95f
Vladislav Kutsobin <kutsobin_vladislav@rambler.ru>
2018-11-12 01:27:25 +0300

    Adding exception

--- src/main/java/app/repository/GenericRepository.java
+++ src/main/java/app/repository/GenericRepository.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface GenericRepository<T, ID> {
     void insert(T t) throws IOException;
-    void delete(ID id);
+    void delete(ID id) throws IOException;
     List<T> findAll() throws FileNotFoundException, IOException;
 
     T getById(ID id) throws IOException;
