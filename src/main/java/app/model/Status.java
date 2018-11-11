2a2dddb56ecb05425bd75f024ae71403478fa631
Vladislav Kutsobin <kutsobin_vladislav@rambler.ru>
2018-11-11 23:30:37 +0300

    Adding test version Status class

---
+++ src/main/java/app/model/Status.java
@@ -0,0 +1,7 @@
+package main.java.app.model;
+
+public class Status {
+    private static final String ACTIVE = "ACTIVE";
+    private static final String RETURNED = "RETURNED";
+    private static final String DELAYED = "DELAYED";
+}
