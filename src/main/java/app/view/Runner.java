e3ee975bb2ac5ef69ab06e50e63415c196fbf24a
Vladislav Kutsobin <kutsobin_vladislav@rambler.ru>
2018-11-12 18:49:18 +0300

    test

--- src/main/java/app/view/Runner.java
+++ src/main/java/app/view/Runner.java
@@ -8,20 +8,11 @@ import java.io.IOException;
 import java.util.Date;
 
 public class Runner {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         FlightRepository flightRepository = new FlightRepositoryImpl();
         Flight flight = new Flight(2, "Moscow", "Barcelona", "Boeing Dreamliner",
                 30, 10, "Nov 13 2018");
-        /*try {
-            flightRepository.insert(flight);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
-        try {
-            System.out.println(flightRepository.searchByDeparture("Moscow"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+        //flightRepository.insert(flight);
+        flightRepository.decreaseClsCapacity(flight, "B");
     }
 }
