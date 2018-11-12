817703efeb440b3aea85dc207dc7951af4593c5e
Vladislav Kutsobin <kutsobin_vladislav@rambler.ru>
2018-11-12 15:49:23 +0300

    remove passenger from passengers list implementation

--- src/main/java/app/repository/io/PassengerRepositoryImpl.java
+++ src/main/java/app/repository/io/PassengerRepositoryImpl.java
@@ -4,6 +4,7 @@ import main.java.app.model.Passenger;
 import main.java.app.repository.PassengerRepository;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class PassengerRepositoryImpl implements PassengerRepository {
@@ -23,7 +24,28 @@ public class PassengerRepositoryImpl implements PassengerRepository {
 
     @Override
     public void delete(Integer integer) throws IOException {
+        File fileWithPassengers = new File(FILE_PASSENGER_PATH);
+        FileReader fr = new FileReader(fileWithPassengers);
+        List<String> passengersList = new ArrayList<>();
+        String line;
 
+        try(BufferedReader br = new BufferedReader(fr)) {
+            while((line = br.readLine())!=null) {
+                String [] splitArray = line.split(",");
+                if(splitArray[0].equals(Integer.toString(integer))) {
+                    continue;
+                } else {
+                    passengersList.add(line);
+                }
+            }
+
+            FileWriter fw = new FileWriter(fileWithPassengers, false);
+            try(BufferedWriter bw = new BufferedWriter(fw)) {
+                for(String str : passengersList) {
+                    bw.write(str + "\n");
+                }
+            }
+        }
     }
 
     @Override
