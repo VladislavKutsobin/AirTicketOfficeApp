090e5c854f5efec4f19b84d6f0c516ead07263bb
Vladislav Kutsobin <kutsobin_vladislav@rambler.ru>
2018-11-14 01:33:53 +0300

    find all tickets method implementation

--- src/main/java/app/repository/io/TicketRepositoryImpl.java
+++ src/main/java/app/repository/io/TicketRepositoryImpl.java
@@ -1,6 +1,10 @@
 package main.java.app.repository.io;
 
+import main.java.app.model.Flight;
+import main.java.app.model.Passenger;
 import main.java.app.model.Ticket;
+import main.java.app.repository.FlightRepository;
+import main.java.app.repository.PassengerRepository;
 import main.java.app.repository.TicketRepository;
 
 import java.io.*;
@@ -52,7 +56,48 @@ public class TicketRepositoryImpl implements TicketRepository {
 
     @Override
     public List<Ticket> findAll() throws FileNotFoundException, IOException {
-        return null;
+        File fileWithTickets = new File(FILE_TICKETS_PATH);
+        FileReader fr = new FileReader(fileWithTickets);
+        List<String> linesList = new ArrayList<>();
+        List<Ticket> ticketsList = new ArrayList<>();
+        String line;
+
+        try(BufferedReader br = new BufferedReader(fr)) {
+            while((line = br.readLine())!=null) {
+                linesList.add(line);
+
+                for(String lineInList : linesList) {
+                    String[] splitLineArray = lineInList.split(",");
+
+                    //--------------------------------------TICKET.ID---------------------------------------------------
+
+                    Integer ticketId = Integer.parseInt(splitLineArray[0]);
+                    String clsInTicket = splitLineArray[3];
+
+                    //--------------------------------------FLIGHT.ID---------------------------------------------------
+
+                    Integer flightId = Integer.parseInt(splitLineArray[1]);
+                    FlightRepository flightRepository = new FlightRepositoryImpl();
+                    Flight flight = flightRepository.getById(flightId);
+
+                    //--------------------------------------PASSENGER.ID------------------------------------------------
+
+                    Integer passengerId = Integer.parseInt(splitLineArray[2]);
+                    PassengerRepository passengerRepository = new PassengerRepositoryImpl();
+                    Passenger passenger = passengerRepository.getById(passengerId);
+
+                    //--------------------------------------------------------------------------------------------------
+
+                    ticketsList.add(new Ticket(
+                            ticketId,
+                            flight,
+                            passenger,
+                            clsInTicket
+                    ));
+                }
+            }
+        }
+        return ticketsList;
     }
 
     @Override
