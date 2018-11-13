c57ef426a09a9db691b73ec7eb9a3a7bfba8e767
Vladislav Kutsobin <kutsobin_vladislav@rambler.ru>
2018-11-14 00:36:04 +0300

    Delete method implementation

--- src/main/java/app/repository/io/TicketRepositoryImpl.java
+++ src/main/java/app/repository/io/TicketRepositoryImpl.java
@@ -4,6 +4,7 @@ import main.java.app.model.Ticket;
 import main.java.app.repository.TicketRepository;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class TicketRepositoryImpl implements TicketRepository {
@@ -25,7 +26,28 @@ public class TicketRepositoryImpl implements TicketRepository {
 
     @Override
     public void delete(Integer integer) throws IOException {
+        File fileWithTickets = new File(FILE_TICKETS_PATH);
+        FileReader fr = new FileReader(fileWithTickets);
+        List<String> ticketList = new ArrayList<>();
+        String line;
 
+        try(BufferedReader br = new BufferedReader(fr)) {
+            while((line = br.readLine())!=null) {
+                String[] splitLineArray = line.split(",");
+                if(splitLineArray[0].equals(Integer.toString(integer))) {
+                    continue;
+                } else {
+                    ticketList.add(line);
+                }
+            }
+        }
+
+        FileWriter fw = new FileWriter(fileWithTickets);
+        try(BufferedWriter bw = new BufferedWriter(fw)) {
+            for(String string : ticketList) {
+                bw.write(string + "\n");
+            }
+        }
     }
 
     @Override
